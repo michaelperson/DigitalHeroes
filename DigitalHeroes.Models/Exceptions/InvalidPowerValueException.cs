c334f5f3be14a49e7e03a1347ea1de05c885369c
michael person <michael.person@cognitic.be>
2025-09-19 09:56:54 +0200

    Ajout des librairies DAL et Models

---
+++ DigitalHeroes.Models/Exceptions/InvalidPowerValueException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalHeroes.Models.Exceptions
+{
+    public class InvalidPowerValueException:Exception
+    {
+        public InvalidPowerValueException():base("La puissance ne peut pas être négative ou dépasser la valeur de 100")
+        {
+            
+        }
+    }
+}
---
+++ DigitalHeroes.Models/Heros.cs
@@ -0,0 +1,124 @@
+﻿using DigitalHeroes.Models.Exceptions;
+
+namespace DigitalHeroes.Models
+{
+    public class Heros
+    {
+        private string _nom;
+        private string _prenom;
+        private string _nomSuperHero;
+        private DateOnly _dateDeNaissance;
+        private DateOnly _dateTransformation;
+        private string _circonstances;
+        private List<string> _pointsFaibles;
+        private int _force;
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return _prenom;
+            }
+
+            set
+            {
+                _prenom = value;
+            }
+        }
+
+        public string NomSuperHero
+        {
+            get
+            {
+                return _nomSuperHero;
+            }
+
+            set
+            {
+                _nomSuperHero = value;
+            }
+        }
+
+        public DateOnly DateDeNaissance
+        {
+            get
+            {
+                return _dateDeNaissance;
+            }
+
+            set
+            {
+                _dateDeNaissance = value;
+            }
+        }
+
+        public DateOnly DateTransformation
+        {
+            get
+            {
+                return _dateTransformation;
+            }
+
+            set
+            {
+                _dateTransformation = value;
+            }
+        }
+
+        public string Circonstances
+        {
+            get
+            {
+                return _circonstances;
+            }
+
+            set
+            {
+                _circonstances = value;
+            }
+        }
+
+        public List<string> PointsFaibles
+        {
+            get
+            {
+                return _pointsFaibles;
+            }
+
+            set
+            {
+                _pointsFaibles = value;
+            }
+        }
+
+        public int Force
+        {
+            get
+            {
+                return _force;
+            }
+
+            set
+            {
+                if(value<0 || value>100)
+                {
+                    throw new InvalidPowerValueException();
+                }
+                _force = value;
+            }
+        }
+    }
+}
